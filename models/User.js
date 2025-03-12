9faf8e143e1f7fe20f9d9d81a54dcfe7436e2f49
Amarildo <amarildokola36@gmail.com>
2025-03-12 21:56:15 +0100

    changed is_validated from boolean to string,just for the moment

--- models/User.js
+++ models/User.js
@@ -3,7 +3,7 @@ const mongoose = require("mongoose");
 const userSchema = new mongoose.Schema({
   name: String,
   role: String,
-  is_validated: Boolean,
+  is_validated: String,
 });
 
 const User = mongoose.model("User", userSchema);
